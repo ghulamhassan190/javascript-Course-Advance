7350376214ca0adda47e54af49d3d29545ba70d3
ghulamhassan99 <hassanshah789567@gmail.com>
2026-03-13 19:11:54 +0500

    update

--- JS-totorial/app.js
+++ JS-totorial/app.js
@@ -58,19 +58,58 @@
 // ====================================================
 //         While Loop & Do While Loop
 // ====================================================
-var resrturent="biryani"
-var cash=1000
-while(cash>0){
-  if (resrturent=="biryani") {
-    console.log('biryani khani 300rupe ki');
-    cash-=300
-    console.log(cash,"cash avaible after eating food");
-    resrturent="andaywalaburger"
-  }
-  else {
-    console.log('andaywalaburger khana 110rupe ka');
-    cash-=110
-    console.log(cash,"cash avaible after eating food");
-    resrturent="biryani"
-  }
-}
+// var resrturent="biryani"
+// var cash=1000
+// while(cash>0){
+//   if (resrturent=="biryani") {
+//     console.log('biryani khani 300rupe ki');
+//     cash-=300
+//     console.log(cash,"cash avaible after eating food");
+//     resrturent="andaywalaburger"
+//   }
+//   else {
+//     console.log('andaywalaburger khana 110rupe ka');
+//     cash-=110
+//     console.log(cash,"cash avaible after eating food");
+//     resrturent="biryani"
+//   }
+// }
+// ========================================================
+                 // Method 2 type 
+  // ======================================================
+// const student={
+//   name:"hassan",
+//   greeting:function(){
+//     console.log('salam');
+    
+//   }
+// }
+// const student2={
+//   name:"hassan",
+//   greeting(){
+//     console.log('salam');
+    
+//   }
+// }
+
+// =================================================
+//                   string Method 
+// =================================================
+// let names="hassan"
+// console.log(names.length);
+// console.log(names.toUpperCase());
+// console.log(names.toLowerCase());
+// console.log(names.charAt(0));
+// console.log(names.charAt(names.length-1));
+// console.log(names.indexOf("n"));
+// console.log(names.trim());
+// console.log(names.includes("hassan"));
+// let friut=["biryani","korma"].slice(1,2)[0][0]
+// console.log(friut);
+// ==================================================
+            // Template Litertals ES6 
+// ==================================================
+
+// console.log('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' + 'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.');
+
+// console.log(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`);

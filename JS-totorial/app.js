2e497a65424d7c8a2c18dfd4a6dcd9601a4b9005
ghulamhassan99 <hassanshah789567@gmail.com>
2026-03-13 06:50:59 +0500

    update app.js

--- JS-totorial/app.js
+++ JS-totorial/app.js
@@ -9,3 +9,47 @@
 //   console.log("Apple is OK");
 //   a="Banana"
 // }
+
+// ============================================
+// Javascript Interivew | Loops | Palindrome
+// ============================================
+// var a=prompt()
+// var b=a.length-1
+// console.log(a,b);
+// var c=''
+
+// for ( i = b; i >=0; i--) {
+//   console.log(a[i]);
+//   c+=a[i]
+//   console.log(c); 
+// }
+
+// if (a==c) {
+//   console.log(" Palindrome value");
+// }else{
+//   console.log(" Not Palindrome value");
+  
+// }
+
+// ==============================================
+       //  Switch statement in javascript 
+// ==============================================
+
+var day="Monday";
+switch(day){
+  case "Monday":
+    console.log("Monday is OK");
+    break;
+  case "Tuesday":
+    console.log("Tuesday is OK");
+    break;
+  case "Wednesday":
+    console.log("Wednesday is OK");
+    break;
+  case "friday":
+    console.log("friday is OK");
+    break;
+  default:
+    console.log("Invalid value");
+    
+}

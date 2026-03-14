8d252761bcbf146976dbfdb96a860a72c2a3a36d
ghulamhassan99 <hassanshah789567@gmail.com>
2026-03-15 00:35:18 +0500

    update app.js

--- JS-totorial/app.js
+++ JS-totorial/app.js
@@ -199,3 +199,8 @@
 // var number=Math.round(Math.random()*5+1)
 // console.log(number);
 
+var parseint="49.4"
+var parsefloat="49.4"
+console.log(parseInt(parseint));
+console.log(parseFloat(parsefloat));
+
